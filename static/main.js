e77e01ed0b0ab46c8b0e583e9358a42ac62f005a
Hang Nguyen <hangnguyennn13@gmail.com>
2019-10-04 08:20:49 +0700

    Change UI-UX

--- static/main.js
+++ static/main.js
@@ -23,6 +23,25 @@ $(function() {
         </li>
         `
     }
+    function renderMusic2(music,index, arr){
+        let name = "s"+index.toString();
+        console.log(name);
+        return `
+        <div id =${name} class="hidden">
+            <div id="${music[3]}" style="display: flex; flex-direction: column;" >
+                <h2 style="color:white">${music[1]}</h2>
+                <p style="color:white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                <p style="color:white">The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to . </p> 
+                <div id ="playlist">
+                    <button id=${index-1} class="preBtn">Previous</button>
+                    <button id=${music[3]} class="curBtn">Play</button>
+                    <button id=${index+1} class="nextBtn">Next</button>
+                </div>
+            </div>
+        </div>
+       
+        `
+    }
 
     $('#form-predict-correction').on('submit', function(e) {
         e.preventDefault();
@@ -41,6 +60,7 @@ $(function() {
             data: form_data,
             success: function(data) {
                 $('#form-predict-correction .button,.section-input-firstname').addClass('hidden')
+                // $('#playerDetail').removeClass("hidden")
                 console.log()
                 $('#output').attr('src', 'data:image/jpg;base64,' + data['img'])
                 // initYoutube();
@@ -49,18 +69,77 @@ $(function() {
                 player.playVideo()
                 console.log(data['videoId'])
                 
-                const articles = data['song'];
-                console.log(data['song'])
-                const articlesHTML = articles.map(renderMusic).join('');
-                document.getElementById('newsList').innerHTML = articlesHTML;
+                // const songs = data['song'];
+                // console.log(data['song'])
+                // const songsHTML = songs.map(renderMusic).join('');
+                // document.getElementById('newsList').innerHTML = songsHTML;
 
                 
+                // document.addEventListener('click', function(e){
+                //     if(e.target.tagName=="BUTTON"){
+                //         player.loadVideoById(e.target.id);
+                //         player.playVideo();
+                //     }
+                //   })
+
+                const songs2 = data['song'];
+                console.log(data['song'])
+                const songsHTML2 = songs2.map(renderMusic2).join('');
+                document.getElementById('playerDetail').innerHTML = songsHTML2;
+                
+                $('#s0').removeClass('hidden');
+
                 document.addEventListener('click', function(e){
                     if(e.target.tagName=="BUTTON"){
-                        player.loadVideoById(e.target.id);
-                        player.playVideo();
+                        if(e.target.className =="preBtn"){
+                            
+                            let preId = parseInt(e.target.id, 10);
+                            if(preId>=0){
+                                
+                                let currId = preId+1;
+                                currId = "s"+currId.toString();
+
+                                // let oldSong = document.getElementById(currId);
+                                // oldSong.addClass("hidden");
+                                $("#"+currId).addClass("hidden");
+
+                                let newId = "s"+preId.toString();
+                                // let newSong = document.getElementById(newId);
+                                // newSong.removeClass("hidden");
+                                $("#"+newId).removeClass("hidden");
+
+                            }
+                        }
+                        if(e.target.className =="nextBtn"){
+                            console.log("nextButton");
+                            let nextId = parseInt(e.target.id, 10);
+                            if(nextId<=9){
+                                let currId = nextId-1;
+                                currId = "s"+currId.toString();
+                                console.log(currId);
+                                // let oldSong = document.getElementById(currId);
+                                // oldSong.addClass("hidden");
+                                $("#"+currId).addClass("hidden");
+
+                                let newId = "s"+nextId.toString();
+                                // let newSong = document.getElementById(newId);
+                                // newSong.removeClass("hidden");
+
+                                $("#"+newId).removeClass("hidden");
+                                console.log(newId);
+
+                            }
+                        }
+                        if(e.target.className =="curBtn"){
+                            
+                            player.loadVideoById(e.target.id);
+                            player.playVideo();
+
+                        
+                        }
                     }
-                  })
+                })
+
             }
         });
     });
